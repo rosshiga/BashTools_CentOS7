e9de68a0769a883c48fdea347a25e011736c0a4d
RossHiga <commits_n_gits@rosshiga.com>
2016-12-25 17:43:52 -1000

    Added certbot for generation of SSL certs

---
+++ setupNGINX.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if ! [ $(id -u) = 0 ]; then
+   echo "Not root!"
+   exit 1
+fi
+
+#Install and enable nginx
+yum -y install nginx certbot
+systemctl start nginx
+systemctl enable nginx
+
+#Allow 80, 443
+firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-service=https
+firewall-cmd --reload
+
+#Remove default index.html
+rm -f /usr/share/nginx/html/index.html
+hostname > /usr/share/nginx/html/index.html
+echo -n " is working" >> /usr/share/nginx/html/index.html
